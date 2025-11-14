f535b9905daa11c2b63bcfff05610d5c455f80e0
stefan-misic <stefan.misic.491@gmail.com>
2025-11-14 14:49:39 +0100

    Config eslint; Add config and env; Add sass

---
+++ src/config.js
@@ -0,0 +1,13 @@
+/* eslint no-undef: "off" */
+const appUrl = new URL(document.URL);
+
+const {
+  VITE_ENV
+} = process.env;
+
+const config = {
+  env: VITE_ENV,
+  appUrl: appUrl
+};
+
+export default config;
