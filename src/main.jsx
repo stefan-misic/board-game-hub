e02630362fd347520a3a8be6cf07cb15e2026f7b
stefan-misic <stefan.misic.491@gmail.com>
2025-11-14 16:03:34 +0100

    Add react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './App.jsx';
 import store from './store/index.js';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
+root.render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </Provider>
 );
