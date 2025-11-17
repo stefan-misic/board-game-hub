25d10bd644422e873c65e7c9b0283a1ef4a18eaa
stefan-misic <stefan.misic.491@gmail.com>
2025-11-17 12:24:47 +0100

    Add react-hook-form

--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,9 +1,21 @@
+import { useForm } from 'react-hook-form';
 import { StyledLayout } from './Layout.styled';
 
 const Layout = ({ children }) => {
+  const { handleSubmit, register } = useForm();
+
+  const handleAppFormSubmit = (appData) => {
+    console.log('appData', appData);
+  };
+
   return (
     <StyledLayout>
       {children}
+
+      <form onSubmit={handleSubmit(handleAppFormSubmit)}>
+        <input {...register('appInput')} />
+        <input type="submit" />
+      </form>
     </StyledLayout>
   );
 };
