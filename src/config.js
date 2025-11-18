b0e7b8c122e7fc239de2bcd7ee42b3098d1ba3b3
stefan-misic <stefan.misic.491@gmail.com>
2025-11-18 13:01:09 +0100

    Add firebase

--- src/config.js
+++ src/config.js
@@ -1,14 +1,23 @@
-/* eslint no-undef: "off" */
-const appUrl = new URL(document.URL);
-
 const {
-  VITE_ENV
-} = process.env;
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_AUTH_DOMAIN,
+  VITE_FIREBASE_PROJECT_ID,
+  VITE_FIREBASE_STORAGE_BUCKET,
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID
+} = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID
+};
 
 const config = {
-  env: VITE_ENV,
-  appUrl: appUrl,
-  servicesDir: '/api'
+  firebaseConfig: firebaseConfig
 };
 
 export default config;
